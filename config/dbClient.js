c0187984bb934321568808c934a6ef0436db9698
Fattah <fatah@gians-MacBook-Pro.local>
2019-09-01 15:51:56 +0700

    setup mongodb

---
+++ config/dbClient.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURL: 'mongodb+srv://fattah:ngawur@cluster0-7fif2.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 6780;
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./config/swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const bodyParser = require('body-parser');
+
+const MongoClient = require('mongodb').MongoClient;
+const dbConfig = require('./config/dbClient');
+const client = new MongoClient(dbConfig.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+var db
+
+client.connect(err => {
+    if (err) return console.log(err)
+    console.log("Database Connected successfully");
+    db = client.db("test");
+    client.close();
+});
+
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Welcome'));
+
+app.listen(port, () => {
+	console.log(`App listening on http://localhost:${port}`)
+});
+
+module.exports = app;
